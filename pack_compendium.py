22ab0cdea9588e78a759a3670578ec039722340d
Frederic.meulenijzer <Frederic.meulenijzer@gmail.com>
2022-01-22 20:40:42 +0100

    Journal compendiums handled

--- pack_compendium.py
+++ pack_compendium.py
@@ -1,8 +1,11 @@
+from distutils.command.clean import clean
 import json
 import os
 import logging
 import shutil
 import urllib.parse
+from bs4 import BeautifulSoup as bs
+import re
 """
 This script analyze a foundryvtt module then pack all assets
 """
@@ -18,6 +21,7 @@ MODULE_ASSETS_DIR="assets"
 FOUNDRYVTT_BASE_DIR="/Users/ksi/Documents/foundryvtt/Data/"
 FOUNDRY_SRC_WORLD="pfs-prep"
 COMPENDIUM_NAME="pfs-s03-scenes.db"
+COMPENDIUM_TYPE="scenes"
 def load_compendium(location):
     entries = []
     with open(location) as openfileobject:
@@ -30,6 +34,9 @@ def generate_module_from_pack(compendium_items, type, foundry_base, module_base)
         new_compendium=pack_actors(compendium_items,foundry_base, module_base)
     elif type=='scenes':
         new_compendium=pack_scenes(compendium_items,foundry_base, module_base)
+    elif type=='journal':
+        new_compendium=pack_journal(compendium_items,foundry_base, module_base)
+
     else:
         raise NameError('Unsupported compendium type: '+type)
     write_updated_compendium(new_compendium,module_base+'/'+MODULE_PACK_DIR+'/'+COMPENDIUM_NAME)
@@ -48,6 +55,31 @@ def pack_actors(compendium_items,foundry_base, module_base):
         new_item['token']['img']=copy_asset(clean_string(item['token']['img']),foundry_base, module_base)
         packed_compendium.append(new_item)
     return packed_compendium
+def pack_journal(compendium_items,foundry_base, module_base):
+    """
+    read through the compendium_items  
+    - copy images to the module directory
+    - returns a new compendium items with update path
+    """
+    packed_compendium=[]
+    for item in compendium_items:
+        new_item=item
+        print(f"Processing entry named {item['name']}")
+        if 'img' in new_item.keys():
+            if new_item['img'] is not None:
+                new_item['img']=copy_asset(clean_string(item['img']),foundry_base, module_base)
+        packed_compendium.append(new_item)
+        if 'content' in new_item.keys():
+            images=get_images_from_content(new_item['content'])
+            for image in images:
+                new_image=copy_asset(clean_string(image['src']),foundry_base, module_base)
+                new_item['content']=re.sub(image['src'], new_image, new_item['content'])
+    return packed_compendium
+
+def get_images_from_content(content):
+        soup=bs(content, "lxml")
+        return(soup.find_all('img'))
+
 def pack_scenes(compendium_items,foundry_base, module_base):
     """
     read through the compendium_items  
@@ -59,7 +91,6 @@ def pack_scenes(compendium_items,foundry_base, module_base):
         new_item=item
         print(f"Processing entry named {item['name']}")
         if 'img' in new_item:
-            print('OK Corral')
             new_item['img']=copy_asset(clean_string(item['img']),foundry_base, module_base)
         if 'tiles' in new_item :
             for tile in new_item['tiles']:
@@ -78,6 +109,10 @@ def write_updated_compendium(compendium, location):
 def clean_string(image_name):
     """
     removes characters following ? in the image URL 
+    soup=bs(content_value) et ensuite images=soup.find('img') --> on récupère une liste des images qui ressemble à ça:
+    <img height="1290" src="pfs-3-01/Market%20square.png" width="1038"/>
+    Ensuite on peut récupèrer le src avec images('src')
+
     """
     file_name=image_name
     if image_name.find('?') >=0 :
@@ -97,7 +132,7 @@ def main():
     src_compendium=FOUNDRYVTT_BASE_DIR+"worlds/"+FOUNDRY_SRC_WORLD+"/packs/"+COMPENDIUM_NAME
     updated_compendium=[]
     compendium_items=load_compendium(src_compendium)
-    generate_module_from_pack(compendium_items,'scenes', FOUNDRYVTT_BASE_DIR, MODULE_ROOT+'/'+MODULE_NAME)
+    generate_module_from_pack(compendium_items,COMPENDIUM_TYPE, FOUNDRYVTT_BASE_DIR, MODULE_ROOT+'/'+MODULE_NAME)
     # Ensure to create the module directory structure if doesn't exist !! 
 
 
